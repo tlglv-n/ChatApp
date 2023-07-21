7d73f4e9aa79b5158ac2aefa6eca05bc0b304fa1
tlglv-n <zander559@mail.ru>
2023-07-21 16:51:20 +0500

    AuthService, connect Firebase, AuthVC UI

--- ChatApp/Model/User.swift
+++ ChatApp/Model/User.swift
@@ -2,11 +2,16 @@
 //  User.swift
 //  ChatApp
 //
-//  Created by Nurikk T. on 20.07.2023.
+//  Created by Nurikk T. on 21.07.2023.
 //
 
 import Foundation
-
 class User {
+    private let email: String?
+    private let password: String?
     
+    init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
 }
